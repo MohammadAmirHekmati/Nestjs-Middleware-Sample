49d7e4a51152f8349b9268bc16ac992fc3f4407e
MohammadAmir Hekmati <87515827+MohammadAmirHekmati@users.noreply.github.com>
2021-12-06 22:45:56 +0330

    Delete old source code

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import { MiddlewareModule } from './middleware/middleware.module';
-
-
-@Module({
-  imports: [MiddlewareModule]
-})
-export class AppModule {}
